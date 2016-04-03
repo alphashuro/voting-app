c085a55201e3e2ea91575ca9012c6c5dcf392d1a
Alpha Shuro <Alpha Shuro>
2016-04-03 22:02:31 +0200

    add optional data param

--- app/common/ajax-functions.js
+++ app/common/ajax-functions.js
@@ -13,16 +13,28 @@ var ajaxFunctions = {
 
       document.addEventListener('DOMContentLoaded', fn, false);
    },
-   ajaxRequest: function ajaxRequest (method, url, callback) {
+   ajaxRequest: function ajaxRequest (method, url, data, callback) {
       var xmlhttp = new XMLHttpRequest();
+      
+      var args = [].slice.call(arguments);
+      args = args.filter(function (val) {
+        return val !== undefined;
+      });
+      
+      method = args.shift();
+      url = args.shift();
+      callback = args.pop();
+
+      data = args.length > 0 ? args.shift() : null; 
 
       xmlhttp.onreadystatechange = function () {
          if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            callback(xmlhttp.response);
+            callback && callback(xmlhttp.response);
          }
       };
 
       xmlhttp.open(method, url, true);
-      xmlhttp.send();
+      data && xmlhttp.setRequestHeader('Content-Type', 'application/json');
+      xmlhttp.send(JSON.stringify(data));
    }
 };
