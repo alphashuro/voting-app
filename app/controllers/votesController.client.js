337ffb2213b5cfb5076eb047eb0975defb954421
Alpha <alphashuro@gmail.com>
2016-04-07 22:25:14 +0200

    display chart for votes

--- app/controllers/votesController.client.js
+++ app/controllers/votesController.client.js
@@ -1,22 +1,49 @@
 (function() {
     var pollId = window.location.pathname.split('/').pop();
-    var apiUrl = appUrl + `/api/polls/${pollId}/votes`;
+    var apiUrl = appUrl + '/api/polls/'+pollId+'/votes';
 
     var $votesStats = document.getElementById('vote-stats');
     function getVotes(data) {
         var votes = JSON.parse(data);
+				console.log(votes);
         var totals = votes.reduce(function(previous, current) {
             previous[current.option] = previous[current.option] ? previous[current.option] + 1 : 1;
             return previous;
         }, {});
         if ($votesStats) {
-            for (var total in totals) {
-                if (totals.hasOwnProperty(total)) {
-                    var $vote = document.createElement('p');
-                    $vote.innerText = `${total}: ${totals[total]}`;
-                    $votesStats.appendChild($vote);
-                }
-            }
+						 // Load the Visualization API and the corechart package.
+						google.charts.load('current', {'packages':['corechart']});
+
+						// Set a callback to run when the Google Visualization API is loaded.
+						google.charts.setOnLoadCallback(drawChart);
+
+						// Callback that creates and populates a data table,
+						// instantiates the pie chart, passes in the data and
+						// draws it.
+						function drawChart() {
+
+							// Create the data table.
+							var data = new google.visualization.DataTable();
+							data.addColumn('string', 'Option');
+							data.addColumn('number', 'Votes');
+							var rows = [];
+							for (var prop in totals) {
+								if (totals.hasOwnProperty(prop)) {
+									rows.push([prop, totals[prop]]);
+								}
+							}
+							data.addRows(rows);
+
+							// Set chart options
+							var options = {'title':'Current Vote Stats',
+														'width':400,
+														'height':300};
+
+							// Instantiate and draw our chart, passing in some options.
+							var chart = new google.visualization.PieChart($votesStats);
+							chart.draw(data, options);
+						}
+
         }
     }
     ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, getVotes));
@@ -26,7 +53,7 @@
         voteForm.addEventListener('submit', function voteHandler(e) {
             e.preventDefault();
             ajaxFunctions.ajaxRequest('POST', apiUrl, { option: this.options.value }, function(res) {
-                console.log(res);
+                window.location.reload();
             });
         });
     }
